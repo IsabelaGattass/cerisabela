3cb22cc78c83339e827581e71b94e3e531363e5b
IsabelaGattass <isabelagattass267@gmail.com>
2025-06-23 11:46:15 -0400

    fiz o produto ser cadastrado e voltar para a pagina de tabela

--- processa_produto.php
+++ processa_produto.php
@@ -11,24 +11,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $produto->setUnidade(filter_input(INPUT_POST, 'unidade', FILTER_SANITIZE_ADD_SLASHES));
 
     if (isset($_POST['btnGravar'])) {
-        // Cadastro de novo produto
+
         if ($produto->add()) {
-            echo "<script>alert('Produto cadastrado com sucesso.'); window.location.href='CadastroProduto.php';</script>";
+            echo "<script>alert('Produto cadastrado com sucesso.'); window.location.href='Produtos.php';</script>";
         } else {
             echo "<script>alert('Erro ao adicionar Produto.'); window.location.href='CadastroProduto.php';</script>";
         }
     } elseif (isset($_POST['btnAlterar'])) {
-        // Edição de produto existente
+      
         $idProduto = filter_input(INPUT_POST, 'idProduto', FILTER_SANITIZE_NUMBER_INT);
-        if ($produto->update($idProduto)) {
+        if ($Produto->update($idProduto)) {
             echo "<script>alert('Produto alterado com sucesso.'); window.location.href='Produtos.php';</script>";
         } else {
             echo "<script>alert('Erro ao alterar Produto.'); window.location.href='Produtos.php';</script>";
         }
     } elseif (isset($_POST['btnDeletar'])) {
-        // Exclusão de produto
+    
         $idProduto = filter_input(INPUT_POST, 'idProduto', FILTER_SANITIZE_NUMBER_INT);
-        if ($produto->delete($idProduto)) {
+        if ($Produto->delete($idProduto)) {
             echo "<script>alert('Produto excluído com sucesso.'); window.location.href='Produtos.php';</script>";
         } else {
             echo "<script>alert('Erro ao excluir Produto.'); window.location.href='Produtos.php';</script>";
