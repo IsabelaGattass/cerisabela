668123a1515e944c8b416bb6d2d4109471eccddd
celsonio <celsosilvaifro@gmail.com>
2025-07-04 09:13:59 -0400

    alterações no index e footer e nav

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/layout.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/baseSite.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="images/logosite.png.png" type="image/x-icon">
     <title>Cerisabela - Software de Programação</title>
 </head>
@@ -12,16 +14,37 @@
 <body>
     <!-- nosso cabeçalho -->
     <header>
-        <h1>CERISABELA</h1>
-        <p>Especializada em Software de Programação</p>
+        <img src="images/logosite.png.png" alt="Logo da Empresa">
+        <div class="empresa">
+            <h1>CERISABELA</h1>
+            <p>Especializada em Software de Programação</p>
+        </div>
     </header>
 
     <!-- menu de navegação -->
-    <nav>
-        <a href="#sobre">Sobre</a>
-        <a href="#serviços">Atividades</a>
-        <a href="#estrutura">Nossa Estrutura</a>
-        <a href="#contato">Contato</a>
+    <nav class="navbar navbar-expand-lg nav-custom" data-bs-theme="dark">
+        <div class="container-fluid">
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="bi bi-three-dots-vertical fs-5"></i>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#sobre">Sobre</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#servicos">Atividades</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#estrutura">Nossa Estrutura</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contato">Contato</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
     <main>
@@ -30,63 +53,58 @@
         </div>
 
         <section id="sobre">
-            <div class="container">
+            <h2>Sobre Nós</h2>
+            <div class="texto">
                 <img src="images/infofine.png" alt="Sobre a empresa">
-                <div class="texto">
-                    <h2>Sobre Nós</h2>
-                    <p>Somos uma empresa referência na criação de softwares de programação, utilizando práticas
-                        tecnológicas robustas e desenvolvimentos de excelência.</p>
-                </div>
+                <p>Somos uma empresa referência na criação de softwares de programação, utilizando práticas
+                    tecnológicas robustas e desenvolvimentos de excelência.</p>
             </div>
 
         </section>
 
         <section id="servicos">
-            <div class="container">
+            <h2>Atividades</h2>
+            <div class="texto">
                 <img src="images/okattfine.png" alt="Atividades da empresa">
-                <div class="texto">
-                    <h2>Atividades</h2>
-                    <ul>
-                        <li>Serviços de Desenvolvimento de Software.</li>
-                        <li>Serviços de Manutenção e Suporte.</li>
-                        <li>Consultoria e Auditoria de Software.</li>
-                        <li>Cibersegurança e Conformidade.</li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>Serviços de Desenvolvimento de Software.</li>
+                    <li>Serviços de Manutenção e Suporte.</li>
+                    <li>Consultoria e Auditoria de Software.</li>
+                    <li>Cibersegurança e Conformidade.</li>
+                </ul>
             </div>
 
         </section>
 
-        </section id="estrututa">
-        <div class="container">
-            <img src="images/gearok.png" alt="Estrutura da empresa">
+        <section id="estrututa">
+            <h2>Nossa Estrutura</h2>
             <div class="texto">
-                <h2>Nossa Estrutura</h2>
+                <img src="images/gearok.png" alt="Estrutura da empresa">
                 <p>Contamos com trabalho de qualidade, laboratórios modernos, áreas de tecnologia e gestão eficiente.
                 </p>
             </div>
-        </div>
 
-        <section>
+        </section>
 
-            <section id="contato">
-                <div class="container">
-                    <img src="images/ctt_preto.png" alt="Contato da empresa">
-                    <div class="texto">
-                        <h2>Contato</h2>
-                        <p>Email: contato@cerisabela.com</p>
-                        <p>Telefone: (99) 99999-9999</p>
-                    </div>
+        <section id="contato">
+            <h2>Contato</h2>
+            <div class="texto">
+                <img src="images/ctt_preto.png" alt="Contato da empresa">
+                <div>
+                    <p>Email: contato@cerisabela.com</p>
+                    <p>Telefone: (99) 99999-9999</p>
                 </div>
+            </div>
 
-            </section>
-            <!-- Finalizamos o main -->
+        </section>
+        <!-- Finalizamos o main -->
     </main>
 
     <footer>
-        <p>&copy; 2025 CERISABELA - Todos os direitos reservados.</p>
+        <?php require_once "_parts/_footer.php" ?>
     </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
