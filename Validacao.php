dfbd4d3f24cafb7daa33fcaded3edd5e35e2bfd1
Your Name <you@example.com>
2025-07-07 09:52:46 -0400

    alteração minima

--- Validacao.php
+++ Validacao.php
@@ -1,15 +1,25 @@
 <?php
+// inicio da sessão
 session_start();
 
+// confirma se o envio da página foi realmente feito com botao
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+    // pega o que foi digitado nos campos das telas de login (email, senha)
     $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
     $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
+    // ativa o carregamento automático d classes. quando criar um novo objeto com new, ele vai buscar o arquivo correspondente 
     spl_autoload_register(function ($class) {
         require_once "Classes/{$class}.class.php";
     });
 
-    $Login = new Usuario();
+
+    // cria um objeto novo da classe Usuário
+    $Login = new Usuario(); 
+
+    
     $dados = $Login->ValidarLogin($email);
     
 
