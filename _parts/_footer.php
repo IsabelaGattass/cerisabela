590e5f42461f3ce6abbdb78fdb5a6f790d2a6a76
celsonio <celsosilvaifro@gmail.com>
2025-08-25 16:10:52 -0400

    OI

--- _parts/_footer.php
+++ _parts/_footer.php
@@ -8,7 +8,7 @@
         <P><i class="bi bi-telephone"></i>(69) 99999-9999</P>
     </div>
     <div class= "r-linha-item rede-social">
-        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.facebook.com/" class="facebook" target="_blank"><i class="bi bi-facebook"></i></a>
         <a href="https://www.instagram.com/" class="instagram" target="_blank"><i class="bi bi-instagram"></i></a>
         <a href="https://web.whatsapp.com/" class="whatsapp" target="_blank"><i class="bi bi-whatsapp"></i></a>
     </div>
