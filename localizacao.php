c0d0ea9a37f821f34cde956276e8a22ddea56c4f
Isabela Gattass <isabelagattass267@gmail.com>
2025-09-08 11:06:36 -0400

    mexendo na localização

--- localizacao.php
+++ localizacao.php
@@ -11,9 +11,8 @@
   <link rel="stylesheet" href="CSS/base.css" />
   <link rel="shortcut icon" href="images/logo.png" type="image/x-icon" />
   <!-- Seu CSS -->
-  <link rel="stylesheet" href="css/home.css">
+  <link rel="stylesheet" href="css/index.css">
    <link rel="stylesheet" href="CSS/baseSite.css" />
-     <link rel="stylesheet" href="_parts/menu.css">
 </head>
 <body>
 
@@ -22,8 +21,8 @@
   </header>
 
 <div class="bloco-fundo-vitaminas largura-total">
-    <div class="conteudo-vitaminas">
-        <h1 class="titulo-vitaminas">
+    <div class="conteudo-esmaltes">
+        <h1 class="titulo-esmaltação">
             <span class="subtitulo-vitaminas">CERISABELA</span><br>
             ESMALTES E ESMALTAÇÕES
         </h1>
@@ -65,7 +64,7 @@
   <div class="map-iframe">
     <iframe 
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6575.7818512287395!2d-61.382980760601235!3d-11.481131343266814!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93c826519e7eca21%3A0x9c3ad57fa8cc996d!2sInstituto%20Federal%20de%20Rond%C3%B4nia%20-%20C%C3%A2mpus%20Cacoal!5e0!3m2!1spt-BR!2sbr!4v1756137156568!5m2!1spt-BR!2sbr"
-      width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" 
+      width="20%" height="100%" style="border:0;" allowfullscreen="" loading="lazy" 
       referrerpolicy="no-referrer-when-downgrade">
     </iframe>
   </div>
@@ -84,7 +83,7 @@
   </footer>
   <!-- Bootstrap JS -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="_parts/menu.js"></script>
+  <script src="_parts/_menu.php"></script>
 
   
 </body>
