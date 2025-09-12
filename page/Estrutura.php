2e4b833652b192acf36586f3ca49f7c5b2d25134
Isabela Gattass <isabelagattass267@gmail.com>
2025-09-12 08:14:06 -0400

    .

--- page/Estrutura.php
+++ page/Estrutura.php
@@ -1,6 +1,6 @@
 <h2>Nossa Estrutura</h2>
 <div class="texto">
     <img src="../images/estrututa.png" alt="Estrutura da empresa">
-    <p>Contamos com trabalho de qualidade, laboratórios modernos, áreas de tecnologia e gestão eficiente.
+    <p>Contamos com trabalho de qualidade, salão moderno e gestão eficiente.
     </p>
 </div>
