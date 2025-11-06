f1d9e5e300dd6d4263bd0479f602519e0e6d4df3
ericateles <ericapriscilasf@gmail.com>
2025-11-06 14:05:52 -0400

    algumas mudanças

--- Validacao.php
+++ Validacao.php
@@ -1,38 +1,12 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+ 
 
-session_start();
-
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-
-
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_STRING);
-    $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
-
-    spl_autoload_register(function ($class) {
-        require_once "Classes/{$class}.class.php";
-    });
-
-   
-    $Login = new Usuario(); 
-    $Login->setsenha($senha);
-
-    $dados = $Login->ValidarLogin($email);
-    
-
-    if (password_verify($senha, $dados->senha)) {
-        $_SESSION['email'] = $dados->nome;
-        
-        header("Location: index.php");
-        exit();
-    } else {
-         echo "<script>window.alert('Bem-vindo, usuário!'); window.location.href='LoginUsuario.php';</script>";
-    }
-} else {
-    header("Location: index.php");
+if (!isset($_SESSION['user_id'])) {
+    $redirect = basename($_SERVER['SCRIPT_NAME'], '.php');
+    header("Location: login.php?redirect=$redirect.php");
     exit;
 }
 
-?>
